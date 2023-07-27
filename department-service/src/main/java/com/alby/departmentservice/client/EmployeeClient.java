b8add2aa69de0111262f8d2b14c38e2bdd4cb4f3
Albin Baby Peedieakkal <39522030+alby268@users.noreply.github.com>
2023-07-26 23:36:11 -0400

    Add files via upload

---
+++ department-service/src/main/java/com/alby/departmentservice/client/EmployeeClient.java
@@ -0,0 +1,16 @@
+package com.alby.departmentservice.client;
+import com.alby.departmentservice.model.Employee;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+@HttpExchange
+public interface EmployeeClient {
+
+    @GetExchange("/employee/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId);
+
+}
